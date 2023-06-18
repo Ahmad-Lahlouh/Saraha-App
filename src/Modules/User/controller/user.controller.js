3b85b782f30dd70ba167f5afc463d20fe4d73317
Ahmad-Lahlouh <ahmd.lh98@gmail.com>
2023-06-18 20:38:38 +0200

    usermodel

--- src/Modules/User/controller/user.controller.js
+++ src/Modules/User/controller/user.controller.js
@@ -1,5 +1,5 @@
 
-// import userModel from "../../../../DB/model/user.model.js"
+import userModel from "../../../../DB/model/user.model.js"
 import cloudinary from "../../../Services/cloudinary.js"
 
 export const profile = (req,res)=>{
